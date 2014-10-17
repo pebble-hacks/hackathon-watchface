84ba7e497845792e9de479f74048114b42610772
Cherie Williams <cherie@getpebble.com>
2014-10-17 15:00:31 -0400

    Created Pebble.js generic watchface

---
+++ src/app.js
@@ -0,0 +1,25 @@
+var UI = require('ui');
+var Vector2 = require('vector2');
+
+var wind = new UI.Window({
+  fullscreen: true
+});
+
+var time_text = new UI.TimeText({
+  position: new Vector2(0, 144),
+  size: new Vector2(144, 24),
+  font: 'gothic-18-bold',
+  text: '%H:%M:%S',
+  textAlign: 'center'
+});
+
+var hackathon_logo = new UI.Image({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 144),
+  backgroundColor: 'clear',
+  image: 'images/pebble.png',
+});
+
+wind.add(hackathon_logo);
+wind.add(time_text);
+wind.show();
